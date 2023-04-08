30d8d8591a0c38ceae4209e5d0990f8eaccf60b1
niskanjo <niskanjo@arcada.fi>
2023-04-08 20:50:49 +0300

    Split header and footer into separate files

---
+++ main.js
@@ -0,0 +1,8 @@
+// Main script file
+console.log("main.js init");
+
+// Fetch header & footer
+$(function() {
+    $('#header').load('../components/header.html');
+    $('#footer').load('../components/footer.html');
+});
